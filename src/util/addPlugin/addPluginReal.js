dd23a9a5809cff35ee89b7dd08b80d2978727789
Zhou Zhongyuan <yawenzhongyuan@gmail.com>
2017-09-04 11:11:23 +0800

    Delete console.log

--- src/util/addPlugin/addPluginReal.js
+++ src/util/addPlugin/addPluginReal.js
@@ -3,7 +3,6 @@ import { cordova } from 'cordova-lib';
 function addPluginReal(plugin,variable){
     return new Promise(function (resolve, reject) {
         cordova.plugin('add', plugin, variable,{'verbose': true},function (err, data) {
-            console.log(plugin, variable);
             if (err) {
                 console.error(err.stack)
                 reject(new Error(err))
